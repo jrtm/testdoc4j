839b22637f577af7fd260924ef8c78517c5640ab
Thomas Flemming <thomas.flemming@usit.uio.no>
2011-03-07 13:47:17 +0000

    Simplified basic example.

--- testdoc/src/main/java/no/uio/tools/testdoc/examples/BasicExample.java
+++ testdoc/src/main/java/no/uio/tools/testdoc/examples/BasicExample.java
@@ -5,28 +5,14 @@ import no.uio.tools.testdoc.TestDocTask;
 import no.uio.tools.testdoc.TestDocTasks;
 import no.uio.tools.testdoc.TestDocTest;
 
-@TestDocPlan("Overview of test")
-// @TestDocWebDavUrl("https://www-dav.uio.no/testplan.html")
+@TestDocPlan("Authentication tests")
 public class BasicExample {
 
-    public static void toBeImplemented() {
-    }
-
-
     @TestDocTest("Test user login")
-    @TestDocTasks({ @TestDocTask(task = "Go to login page", checks = "Check 1"),
-            @TestDocTask(task = "Task 2", checks = { "Check1", "Check2" }) })
+    @TestDocTasks({ @TestDocTask(task = "Go to login page", checks = "Check 1") })
     public void testUserLogin() {
-        int i = 10;
-        i = i + 2;
-    }
-
-
-    @TestDocTest("Test user logout")
-    @TestDocTasks({ @TestDocTask(task = "Click logout link", checks = "Look for goodbye message") })
-    public void testUserLogout() {
-        int i = 10;
-        i = i + 2;
+        // Testcode here
+        assert (true);
     }
 
 }
